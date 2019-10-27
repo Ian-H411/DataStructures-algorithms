f8a8e21b83ec633fe6890c405e861699366b2f24
Ian_H411 <ian.h411@gmail.com>
2019-10-27 14:42:41 -0600

    completes stack file and adds to the repository

--- Stack.playground/Contents.swift
+++ Stack.playground/Contents.swift
@@ -1,3 +1,25 @@
 import Cocoa
 
-var str = "Hello, playground"
+public struct Stack<T> {
+    
+    fileprivate var array = [T]()
+    
+    public var count: Int {
+        return array.count
+    }
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public mutating func push(_ item: T) {
+        array.append(item)
+    }
+    public mutating func pop() -> T? {
+        return array.popLast()
+    }
+    
+    public mutating func top() -> T? {
+        return array.last
+    }
+}
