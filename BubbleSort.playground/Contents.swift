bce992e63ba953e0d9c02eba66c9a437610c3cfc
Ian_H411 <ian.h411@gmail.com>
2019-11-07 21:15:40 -0700

    adds a secondary bubblesort that is more swifty

--- BubbleSort.playground/Contents.swift
+++ BubbleSort.playground/Contents.swift
@@ -23,3 +23,17 @@ func bubbleSort(array: [Int]) -> [Int]{
 print(bubbleSort(array: [9,920,5,7,4,3,56,9,5,3,21]))
 
 
+//same theory but uses the swift swapat
+func bubble2(array: [Int]) {
+    var array = array
+    for i in 0..<array.count {
+        for j in 1..<array.count - i {
+            if array[j] < array[j - 1] {
+                array.swapAt(j, j - 1)
+            }
+        }
+    }
+    print(array)
+}
+
+bubble2(array: [3,4,2,1,3,5])
