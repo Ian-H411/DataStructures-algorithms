ebaf35942786ae3823843dce8284838548913c43
Ian_H411 <ian.h411@gmail.com>
2019-11-07 21:09:32 -0700

    adds bubblesort

--- BubbleSort.playground/Contents.swift
+++ BubbleSort.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var str = "Hello, playground"
+//average sort complexity = N^2
+//worst is the same
+
+//space complexity is 1
+
+//basic bubbleSort
+func bubbleSort(array: [Int]) -> [Int]{
+    var array = array
+    for i in 0..<array.count {
+        for j in 1..<array.count - i {
+            if array[j] < array [j - 1] {
+                let temporary = array[j - 1]
+                array[j - 1] = array[j]
+                array[j] = temporary
+            }
+        }
+    }
+    return array
+}
+
+print(bubbleSort(array: [9,920,5,7,4,3,56,9,5,3,21]))
+
+
