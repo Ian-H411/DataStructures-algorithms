6c356aba9442627f609c2f28e575ffd74d272bce
Ian_H411 <ian.h411@gmail.com>
2019-11-15 11:16:37 -0700

    adds examples and finishes the BST

--- BinarySearchTree.playground/Contents.swift
+++ BinarySearchTree.playground/Contents.swift
@@ -146,6 +146,73 @@ public class BinarySearchTreeNode<T: Comparable> {
         rightNode = nil
         return replacement
     }
+    
+    public func height() -> Int {
+        if isLeaf {
+            return 0
+        } else {
+            return 1 + max(leftNode?.height() ?? 0, rightNode?.height() ?? 0)
+        }
+    }
 }
 
+let tree = BinarySearchTreeNode(array: [5,23,4,6,1,1,2,3,43,4,2,2,3,4,5,6,7,7,7])
+
+let value = tree.search(value: 43)
+
+if let val = value {
+    print(val.value)
+}
+
+
+//now with a simple class
+
+class Dog {
+    let breed: String
+    let name: String
+    let age: Int
+    
+    init(name: String, breed: String, age: Int) {
+        self.breed = breed
+        self.name = name
+        self.age = age
+    }
+    
+}
+
+extension Dog: Comparable {
+    static func < (lhs: Dog, rhs: Dog) -> Bool {
+        return lhs.age < rhs.age
+    }
+    
+    static func == (lhs: Dog, rhs: Dog) -> Bool {
+        return lhs.age == rhs.age
+    }
+    
+    
+}
+
+func createRandomDogs(howMany: Int) -> [Dog] {
+    var array: [Dog] = []
+    for _ in 0...howMany {
+        let dog = Dog(name: UUID().uuidString, breed: UUID().uuidString, age: Int.random(in: 0...15))
+            array.append(dog)
+    }
+    return array
+}
+
+let dogTree = BinarySearchTreeNode(array: createRandomDogs(howMany: 54))
+
+print(dogTree.height())
+
+let dog = dogTree.search(value: Dog(name: "bart", breed: "yeet", age: 4) )
+
+//since the dogs age is random sometimes we will not find a dog but chances are low due to how many were creating
+
+if let dog = dog {
+    print(dog.value.age)
+    print(dog.value.name)
+} else {
+    print("no dog found")
+}
 
