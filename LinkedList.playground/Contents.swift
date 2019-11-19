7398ba581d27323e8d52b6482b52e087e9994c8b
Ian_H411 <ian.h411@gmail.com>
2019-11-18 22:56:56 -0700

    adds some properties to the linkedlistnodeclass

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -1,4 +1,11 @@
 import UIKit
 
-var str = "Hello, playground"
-
+public class LinkedListNode<T> {
+    var value: T
+    var next: LinkedListNode?
+    weak var previous: LinkedListNode?
+    
+    public init(value: T) {
+        self.value = value
+    }
+}
