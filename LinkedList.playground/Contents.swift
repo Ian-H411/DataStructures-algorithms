7f2c6dfa4a9ed8a5399b62c04d5b2504a76c0604
Ian_H411 <ian.h411@gmail.com>
2019-11-19 21:59:06 -0700

    adds first and last functions

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -9,3 +9,27 @@ public class LinkedListNode<T> {
         self.value = value
     }
 }
+
+public class LinkedList<T> {
+    public typealias Node = LinkedListNode<T>
+    
+    private var head: Node?
+    
+    public var isEmpty: Bool {
+        return head == nil
+    }
+    
+    public var first: Node? {
+        return head
+    }
+    
+    public var last: Node? {
+        guard var node = head else {
+            return nil
+        }
+        while let next = node.next {
+            node = next
+        }
+        return node
+    }
+}
